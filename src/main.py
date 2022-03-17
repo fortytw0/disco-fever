1778c4ce98e1aefb6c076c2bf50b0f30ad58c4ca
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-03-16 20:02:40 -0600

    Added a couple of lines to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from src.retriever.GoldenRetriever import GoldenRetriever
+from src.encoder.Zencoder import Zencoder
 
 
 golden_retriever = GoldenRetriever(db_path='data/feverous_wikiv1.db', mode='train', model_name='default')
-fetched_entites = golden_retriever.retrieve('J. K. Rowling wrote Harry Potter')
+zencoder = Zencoder()
+
+claim = 'J. K. Rowling wrote Harry Potter'
+fetched_entites = golden_retriever.retrieve(claim)
+zencoder.encode(claim, fetched_entites)
 
 print(fetched_entites)
 print(type(fetched_entites))
